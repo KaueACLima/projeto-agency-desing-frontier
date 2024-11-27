ba372d80a8e5002352067bc0e2b6a72c978eb897
Kauê Lima <56000639+KaueACLima@users.noreply.github.com>
2024-11-26 23:38:58 -0300

    Update CSS/index.css

--- assets/php/index.php
+++ assets/php/index.php
@@ -1,12 +1,12 @@
 <?php
-    $nome = addcslashes($_POST['nome']);
-    $email = addcslashes($_POST['email']);
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
     
-    $assunto = addcslashes($_POST["assunto"]);
+    $assunto = $_POST["assunto"];
     $too = "agenciadesingfrontier@gmail.com";
-    $comentario = addcslashes($_POST["comentario"]);
+    $comentario = $_POST["comentario"];
     
-    $corpoEmail = "Nome: ".$nome."\n"."E-mail: ".$email."\n"."Assunto".$assunto
+    $corpoEmail = "Nome: ".$nome."\n"."E-mail: ".$email."\n"."Assunto".$assunto;
     $cabecalho = "From agenciadesingfrontier@gmail.com"."\n"."Replay-to ".$email."\n"."X=Mailer:PHP/".phpversion();
 
     if (mail ($too, $assunto, $corpoEmail, $cabecalho)) {
@@ -15,19 +15,3 @@
         echo("Houve um erro ao enviar o email.");
     }
 ?>
-
-<!-- <script>
-window.onload = function() {
-  Swal.fire({
-    title: 'Obrigado !',
-    text: 'Agradecemos ao seu feedback, é importante para nós. Em até de 24h receberá sua resposta.',
-    icon: 'success',
-    confirmButtonText: 'Ok'
-  });
-}
-</script>
-
-<a href="javascript:history.go(-1)">Voltar para a página anterior</a>
-</body>
-
-</html> -->
