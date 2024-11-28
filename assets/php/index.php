660051c2a92946a5fe1ba77b106a3752810fd762
Kauê Lima <56000639+KaueACLima@users.noreply.github.com>
2024-11-27 22:21:10 -0300

    Update CSS/index.css

--- assets/php/index.php
+++ assets/php/index.php
@@ -1,17 +1,37 @@
-<?php
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!--* CSS  -->
+  <link rel="stylesheet" href="../css/page-php.css">
+  <title>Agradecemos seu feedback</title>
+</head>
+
+<body>
+
+  <?php
     $nome = $_POST['nome'];
     $email = $_POST['email'];
-    
     $assunto = $_POST["assunto"];
-    $too = "agenciadesingfrontier@gmail.com";
     $comentario = $_POST["comentario"];
-    
-    $corpoEmail = "Nome: ".$nome."\n"."E-mail: ".$email."\n"."Assunto".$assunto;
-    $cabecalho = "From agenciadesingfrontier@gmail.com"."\n"."Replay-to ".$email."\n"."X=Mailer:PHP/".phpversion();
-
-    if (mail ($too, $assunto, $corpoEmail, $cabecalho)) {
-        echo("Agradecemos seu feedback. Seu e-mail foi enviado com sucesso!");
-    } else {
-        echo("Houve um erro ao enviar o email.");
-    }
+    $cabecalho = "De agenciadesingfrontier@gmail.com"."\n"."X=Mailer:PHP/".phpversion();
 ?>
+
+
+  <div>
+    <h1>Seu e-mail foi enviado com sucesso!</h1>
+    <p>Em até 24h seu e-mail será respondido. Nossa equipe agradece o seu feedback.</p>
+
+    <h2>Corpo do email:</h2>
+    <p>Nome: ".$nome."</p><br>
+    <p>Nome: ".$email."</p><br>
+    <p>Nome: ".$assunto."</p><br>
+    <p>Nome: ".$comentario."</p><br>
+    <p>$cabecalho</p>
+  </div>
+</body>
+
+</html>
